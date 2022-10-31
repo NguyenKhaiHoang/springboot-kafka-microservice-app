1f29d0ef0f18f791f7fb14fdf16b7c04969278f4
khaihoang.nguyen <hoangnguyen.ie12@gmail.com>
2022-10-31 15:31:50 +0700

    feat: add notificationservice

---
+++ notificationservice/src/main/java/hoangnguyen/io/notificationservice/model/MessageDTO.java
@@ -0,0 +1,12 @@
+package hoangnguyen.io.notificationservice.model;
+
+import lombok.Data;
+
+@Data
+public class MessageDTO {
+    private String to;
+    private String toName;
+    private String subject;
+    private String content;
+
+}
